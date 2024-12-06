51041f4c886625a3a2e24610713e44bb32fc792a
Ahmed Misbah <ahmed.misbah@arabiyya.edu.mv>
2024-12-06 18:16:31 +0500

    Enable cors on backend

--- noticeboard-backend/src/main.ts
+++ noticeboard-backend/src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.CORS_ORIGINS?.split(','),
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Noticeboard API')
