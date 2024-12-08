3c215f33ab4600d677b601f3e1f5051747e7de1e
Ahmed Misbah <ahmed.misbah@arabiyya.edu.mv>
2024-12-08 16:41:16 +0500

    import the modules

--- noticeboard-backend/src/system/system.module.ts
+++ noticeboard-backend/src/system/system.module.ts
@@ -4,9 +4,10 @@ import { SystemService } from './system.service';
 import { VideoModule } from '../video/video.module';
 import { ScheduleModule } from '../schedule/schedule.module';
 import { CountdownModule } from '../countdown/countdown.module';
+import { WebsocketModule } from 'src/shared/websocket/websocket.module';
 
 @Module({
-  imports: [VideoModule, ScheduleModule, CountdownModule],
+  imports: [VideoModule, ScheduleModule, CountdownModule, WebsocketModule],
   controllers: [SystemController],
   providers: [SystemService],
   exports: [SystemService],
